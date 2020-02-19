5a59f0af39019c4c34a32e3433c6252f1f45b20c
zhonghong <1789592159@qq.com>
2020-02-19 23:07:00 +0800

    添加Food类

---
+++ js/food.js
@@ -0,0 +1,7 @@
+class Food{
+    constructor(){
+        
+    }
+}
+
+export default Food;
---
+++ js/map.js
@@ -0,0 +1,40 @@
+//Map类
+class Map {
+    constructor(el, rect = 10) {
+        this.el = el;
+        this.rect = rect;
+        this.data = [];
+        this.rows = Math.ceil(Map.getStyle(el, "width") / rect);
+        this.cells = Math.ceil(Map.getStyle(el, "height") / rect);
+        Map.setStyle(el, "height", this.rows * this.rect);
+        Map.setStyle(el, "width", this.cells * this.rect);
+    }
+    static getStyle(el, attr) {
+        return parseFloat(getComputedStyle(el)[attr]);
+    }
+    static setStyle(el, attr, val) {
+        el.style[attr] = val + 'px';
+    }
+    setData(newData) {
+        this.data = this.data.concat(newData);
+    }
+    clearData() {
+        this.data.length = 0;
+    }
+    include({x, y}){
+        return this.data.some(item=>item.x==x&&item.y==y);
+    }
+    render() {
+        this.el.innerHTML = this.data.map(item => {
+            return `<span style="
+            position:absolute;
+            left:${item.x * this.rect}px;
+            top:${item.y * this.rect}px;
+            width:${this.rect}px;
+            height:${this.rect}px;
+            background:${item.color};"></span>`;
+        }).join('');
+    }
+}
+
+export default Map;
